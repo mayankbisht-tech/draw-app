e6d701985a748a31b0b320dbb1bce82e6c48bff9
mayankbisht-tech <mayankbisht1107@gmail.com>
2025-08-25 23:18:27 +0530

    Update dotenv 8

--- app/packages/backend_common/src/index.ts
+++ app/packages/backend_common/src/index.ts
@@ -1,8 +1,11 @@
-import module = require("next/dist/server/route-modules/app-route/module");
-
-import dotenv=require('dotenv');
+import dotenv from 'dotenv';  
 
 dotenv.config();
 
-const JWT_SECRET =  process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined in .env");
+}
+
 export { JWT_SECRET };
